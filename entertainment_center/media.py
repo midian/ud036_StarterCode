6637622b362bf976a2d356ec8344e8e9deedda52
gabriel_aquino <gabraquino@gmail.com>
2018-01-02 00:49:27 -0200

    Update media.py

--- entertainment_center/media.py
+++ entertainment_center/media.py
@@ -2,7 +2,16 @@ import webbrowser
 
 class Movie():
 
-    """ This class provides a way to store movie related information """
+    """ This class provides a way to store movie related information
+
+    Attributes:
+        attr1 (str): This attribute represents the movie's title.
+        attr2 (str): This attribute represents the movie's storyline.
+        attr3 (str): This attribute represents the movie's poster image url
+                     With this attribute is possible to show the movie poster.
+        attr4 (str): This attribute represents the movie's youtube url
+                     With this attribute is possible to show the movie trailer.
+    """
 
     def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
         self.title = title
