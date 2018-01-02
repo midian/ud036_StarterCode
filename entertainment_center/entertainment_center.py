49b37c5fe72785b0d20e6d277cfbea50d904a718
gabriel_aquino <gabraquino@gmail.com>
2018-01-02 00:41:43 -0200

    update entertaiment_center.py

--- entertainment_center/entertainment_center.py
+++ entertainment_center/entertainment_center.py
@@ -31,42 +31,42 @@ pulp_fiction = media.Movie("Pulp Fiction",
 
 # Kill Bill Inicialization
 kill_bill = media.Movie("Kill Bill",
-                            "A woman in a wedding dress, the Bride,"+
-                            " lies wounded in a chapel " +
-                            "in El Paso, Texas, having been attacked by" +
-                            " the Deadly Viper Assassination Squad. " +
-                            "She tells their leader, Bill, " +
-                            "that she is pregnant with his baby.",
-                            "http:////upload.wikimedia.org/wikipedia/en/thumb/c/cf/Kill_bill_vol_one_ver.jpg/220px-Kill_bill_vol_one_ver.jpg", #NOQA
-                            "https://www.youtube.com/watch?v=iTCHO0jx2C8")
+                        "A woman in a wedding dress, the Bride,"+
+                        " lies wounded in a chapel " +
+                        "in El Paso, Texas, having been attacked by" +
+                        " the Deadly Viper Assassination Squad. " +
+                        "She tells their leader, Bill, " +
+                        "that she is pregnant with his baby.",
+                        "http:////upload.wikimedia.org/wikipedia/en/thumb/c/cf/Kill_bill_vol_one_ver.jpg/220px-Kill_bill_vol_one_ver.jpg", #NOQA
+                        "https://www.youtube.com/watch?v=iTCHO0jx2C8")
 
 # Reservoir Dogs Inicialization
 reservoir_dogs = media.Movie("Reservoir Dogs",
-                                "Eight men eat breakfast at a Los Angeles" +
-                                " diner before carrying out a diamond heist." +
-                                " Six of them use aliases: Mr. Blonde," +
-                                " Mr. Blue, Mr. Brown, Mr. Orange," +
-                                " Mr. Pink, and Mr. White. ",
-                                "http://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Reservoir_dogs_ver1.jpg/220px-Reservoir_dogs_ver1.jpg", #NOQA
-                                "https://www.youtube.com/watch?v=2KLZ4fSXtgI")
+                             "Eight men eat breakfast at a Los Angeles" +
+                             " diner before carrying out a diamond heist." +
+                             " Six of them use aliases: Mr. Blonde," +
+                             " Mr. Blue, Mr. Brown, Mr. Orange," +
+                             " Mr. Pink, and Mr. White. ",
+                             "http://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Reservoir_dogs_ver1.jpg/220px-Reservoir_dogs_ver1.jpg", #NOQA
+                             "https://www.youtube.com/watch?v=2KLZ4fSXtgI")
 
 # Jackie Brown Inicialization
 jackie_brown = media.Movie("Jackie Brown",
-                                "Jackie Brown is a flight attendant for a small" +
-                                " Mexican airline. To make ends meet, she " +
-                                "smuggles money from Mexico into the " +
-                                "United States for Ordell Robbie, " +
-                                "a black-market gun runner living in the" +
-                                " Los Angeles metropolitan area under the " +
-                                "ATF's close watch, forcing him to " +
-                                "use couriers.",
-                                "http://upload.wikimedia.org/wikipedia/en/thumb/8/80/Jackie_Brown70%27s.jpg/220px-Jackie_Brown70%27s.jpg", #NOQA
-                                "https://www.youtube.com/watch?v=HlAECQzTkfY")
+                           "Jackie Brown is a flight attendant for a small" +
+                           " Mexican airline. To make ends meet, she " +
+                           "smuggles money from Mexico into the " +
+                           "United States for Ordell Robbie, " +
+                           "a black-market gun runner living in the" +
+                           " Los Angeles metropolitan area under the " +
+                           "ATF's close watch, forcing him to " +
+                           "use couriers.",
+                           "http://upload.wikimedia.org/wikipedia/en/thumb/8/80/Jackie_Brown70%27s.jpg/220px-Jackie_Brown70%27s.jpg", #NOQA
+                           "https://www.youtube.com/watch?v=HlAECQzTkfY")
 
 
 # List of movies
 movies = [monsters_inc, donnie_darko, pulp_fiction,
-    kill_bill, reservoir_dogs, jackie_brown]
+          kill_bill, reservoir_dogs, jackie_brown]
 
 # Calls the webpage giving the list of movies as argument.
 fresh_tomatoes.open_movies_page(movies)
