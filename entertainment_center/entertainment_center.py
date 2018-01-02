87254e807af16cf83489674e26b0ff8a1f827007
gabriel_aquino <gabraquino@gmail.com>
2018-01-02 00:40:20 -0200

    update entertaiment_center.py

--- entertainment_center/entertainment_center.py
+++ entertainment_center/entertainment_center.py
@@ -9,8 +9,7 @@ import fresh_tomatoes
 monsters_inc = media.Movie("Monsters, Inc.",
                         "The city of Monstropolis in the monster world" +
                         " is powered by energy from the screams of human children. ",
-                        "http://upload.wikimedia.org/wikipedia"+
-                        "/en/6/63/Monsters_Inc.JPG",
+                        "http://upload.wikimedia.org/wikipedia/en/6/63/Monsters_Inc.JPG", #NOQA
                         "https://www.youtube.com/watch?v=UplAPb2sfbs")
 
 
@@ -20,17 +19,14 @@ donnie_darko = media.Movie("Donnie Darko",
                         " Virginia, troubled teenager Donnie Darko sleepwalks" +
                         " out his house and is woken by a figure in a" +
                         " monstrous rabbit costume.",
-                        "http://upload.wikimedia.org/wikipedia" +
-                        "/en/d/db/Donnie_Darko_poster.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/d/db/Donnie_Darko_poster.jpg", #NOQA
                         "https://www.youtube.com/watch?v=ZZyBaFYFySk")
 
 # Pulp Fiction Inicialization
 pulp_fiction = media.Movie("Pulp Fiction",
                                 "One of the most famous films" +
                                 "of Quentin Tarantino.",
-                                "http://upload.wikimedia.org/wikipedia/pt/" +
-                                "thumb/8/82/Pulp_Fiction_cover.jpg/240px-"+
-                                "Pulp_Fiction_cover.jpg",
+                                "http://upload.wikimedia.org/wikipedia/pt/thumb/8/82/Pulp_Fiction_cover.jpg/240px-Pulp_Fiction_cover.jpg",  #NOQA
                                 "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
 # Kill Bill Inicialization
@@ -41,9 +37,7 @@ kill_bill = media.Movie("Kill Bill",
                             " the Deadly Viper Assassination Squad. " +
                             "She tells their leader, Bill, " +
                             "that she is pregnant with his baby.",
-                            "http:////upload.wikimedia.org/wikipedia/en/" +
-                            "thumb/c/cf/Kill_bill_vol_one_ver.jpg/"+
-                            "220px-Kill_bill_vol_one_ver.jpg",
+                            "http:////upload.wikimedia.org/wikipedia/en/thumb/c/cf/Kill_bill_vol_one_ver.jpg/220px-Kill_bill_vol_one_ver.jpg", #NOQA
                             "https://www.youtube.com/watch?v=iTCHO0jx2C8")
 
 # Reservoir Dogs Inicialization
@@ -53,9 +47,7 @@ reservoir_dogs = media.Movie("Reservoir Dogs",
                                 " Six of them use aliases: Mr. Blonde," +
                                 " Mr. Blue, Mr. Brown, Mr. Orange," +
                                 " Mr. Pink, and Mr. White. ",
-                                "http://upload.wikimedia.org/wikipedia/en/thumb" +
-                                "/f/f6/Reservoir_dogs_ver1.jpg/220px-" +
-                                "Reservoir_dogs_ver1.jpg",
+                                "http://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Reservoir_dogs_ver1.jpg/220px-Reservoir_dogs_ver1.jpg", #NOQA
                                 "https://www.youtube.com/watch?v=2KLZ4fSXtgI")
 
 # Jackie Brown Inicialization
@@ -68,9 +60,7 @@ jackie_brown = media.Movie("Jackie Brown",
                                 " Los Angeles metropolitan area under the " +
                                 "ATF's close watch, forcing him to " +
                                 "use couriers.",
-                                "http://upload.wikimedia.org/wikipedia/" +
-                                "en/thumb/8/80/Jackie_Brown70%27s.jpg/" +
-                                "220px-Jackie_Brown70%27s.jpg",
+                                "http://upload.wikimedia.org/wikipedia/en/thumb/8/80/Jackie_Brown70%27s.jpg/220px-Jackie_Brown70%27s.jpg", #NOQA
                                 "https://www.youtube.com/watch?v=HlAECQzTkfY")
 
 
