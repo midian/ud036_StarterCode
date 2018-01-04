177325f14a5298dd6e9a3e8c238c63121dad5ab0
gabriel_aquino <gabraquino@gmail.com>
2018-01-04 00:41:56 -0200

    Update to be on PEP8 requirements.

--- entertainment_center/media.py
+++ entertainment_center/media.py
@@ -13,16 +13,19 @@ class Movie():
         attr1 (str): This attribute represents the movie's title.
         attr2 (str): This attribute represents the movie's storyline.
         attr3 (str): This attribute represents the movie's poster image url
-                     With this attribute is possible to show the movie poster.
+                     With this attribute is possible to show the
+                     movie poster.
         attr4 (str): This attribute represents the movie's youtube url
-                     With this attribute is possible to show the movie trailer.
+                     With this attribute is possible to show the movie
+                     trailer.
     """
 
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, storyline, poster_image_url,
+                 trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
-         webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_youtube_url)
